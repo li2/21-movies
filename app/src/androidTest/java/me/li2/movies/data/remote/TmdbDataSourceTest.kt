178697d28582eaca8ab46739feede365dcc3a931
Weiyi Li <weiyi.just2@gmail.com>
2021-08-25 20:32:10 +1200

    Added android test for DataSource

---
+++ app/src/androidTest/java/me/li2/movies/data/remote/TmdbDataSourceTest.kt
@@ -0,0 +1,39 @@
+package me.li2.movies.data.remote
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
+import org.hamcrest.MatcherAssert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class TmdbDataSourceTest {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var remote: TmdbDataSource
+
+    @Before
+    fun setup() {
+        remote = TmdbDataSource()
+    }
+
+    @Test
+    fun checkAPIResponseForGetGenres() {
+        runBlocking {
+            val genres = remote.getGenres().genres
+            assertThat(genres.size, CoreMatchers.`is`(CoreMatchers.not(0)))
+            genres.forEach {
+                assertNotNull(it.id)
+                assert(it.name.isNotBlank())
+            }
+        }
+    }
+}
