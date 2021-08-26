b3ffe90e4e7d9c7e19e0a14a18ba572fa5e632ee
Weiyi Li <weiyi.just2@gmail.com>
2021-08-26 17:39:19 +1200

    android test: navigation (failed: id is not expected)

--- app/src/androidTest/java/me/li2/movies/ui/movies/MoviesFragmentTest.kt
+++ app/src/androidTest/java/me/li2/movies/ui/movies/MoviesFragmentTest.kt
@@ -1,8 +1,12 @@
 package me.li2.movies.ui.movies
 
+import androidx.annotation.IdRes
+import androidx.core.os.bundleOf
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.navigation.NavController
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -10,13 +14,16 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.runner.AndroidJUnit4
 import me.li2.movies.R
 import me.li2.movies.ui.widgets.movies.MovieViewHolder
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+
 
 @RunWith(AndroidJUnit4::class)
 class MoviesFragmentTest {
+
+    @IdRes
+    private val theme: Int = R.style.AppTheme
 
     /**
      * Test navigation
@@ -26,22 +33,28 @@ class MoviesFragmentTest {
      * https://stackoverflow.com/questions/56558775/launchfragmentincontainer-unable-to-resolve-activity-in-android
      */
     @Test
-    fun testNavigationToMovieDetailScreen() {
-        // Create a mock NavController
-        val mockNavController = mock(NavController::class.java)
+    fun testMoviesFragmentToMovieDetailScreen() {
+        // Create a TestNavHostController
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
 
         // Create a graphical FragmentScenario for the movies list screen
-        val moviesScenario = launchFragmentInContainer<MoviesFragment>()
+        val args = bundleOf("moviesCategory" to NowPlayingCategory)
+        val moviesScenario = launchFragmentInContainer<MoviesFragment>(args, theme)
 
         // Set the NavController property on the fragment
         moviesScenario.onFragment { fragment ->
-            Navigation.setViewNavController(fragment.requireView(), mockNavController)
+            // Set the graph on the TestNavHostController
+            navController.setGraph(R.navigation.nav_main)
+
+            // Make the NavController available via the findNavController() APIs
+            Navigation.setViewNavController(fragment.requireView(), navController)
         }
 
         // Verify that performing a click prompts the correct Navigation action
         onView(withId(R.id.moviesRecyclerView))
                 .perform(RecyclerViewActions.actionOnItemAtPosition<MovieViewHolder>(0, click()))
-        // todo IllegalStateException: No view holder at position: 0
-        verify(mockNavController).navigate(R.id.showMovieDetail)
+
+        assertEquals(navController.currentDestination?.id, R.id.movieDetailFragment)
+        //verify(navController).navigate(R.id.showMovieDetail)
     }
 }
